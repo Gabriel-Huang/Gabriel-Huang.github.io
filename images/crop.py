373a8cdc64722657879b27e554559aaceb62e032
Gabriel Huang <jiahuih@adobe.com>
2026-05-01 11:24:23 -0700

    updates

--- images/crop.py
+++ images/crop.py
@@ -1,43 +1,43 @@
 import cv2
 
-name = 'motion_canvas'
-video = cv2.VideoCapture(name + '.mp4')
-ret, img = video.read()
+# name = 'dage'
+# video = cv2.VideoCapture(name + '.mp4')
+# ret, img = video.read()
 
-video_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
-video_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-video_size = min(video_width, video_height)
-video_crop = int((video_width - video_size) / 2)
-video_resize = (256, 256)
-img_width = img.shape[1]
-img_height = img.shape[0]
-img_size = min(img_width, img_height)
-img_crop = int((img_width - img_size) / 2)
+# video_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+# video_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+# video_size = min(video_width, video_height)
+# video_crop = int((video_width - video_size) / 2)
+# video_resize = (256, 256)
+# img_width = img.shape[1]
+# img_height = img.shape[0]
+# img_size = min(img_width, img_height)
+# img_crop = int((img_width - img_size) / 2)
 
-# Crop the video to 1:1
-video_crop = video_crop + int((video_size - video_size) / 2)
-frames = []
-while True:
-    ret, frame = video.read()
-    if not ret:
-        break
-    frame = frame[:, video_crop:video_crop + video_size, :]
-    frame = cv2.resize(frame, video_resize)
-    frames.append(frame)
+# # Crop the video to 1:1
+# video_crop = video_crop + int((video_size - video_size) / 2)
+# frames = []
+# while True:
+#     ret, frame = video.read()
+#     if not ret:
+#         break
+#     frame = frame[:, video_crop:video_crop + video_size, :]
+#     frame = cv2.resize(frame, video_resize)
+#     frames.append(frame)
 
-# Crop the image to 1:1
-img = img[:, img_crop:img_crop + img_size, :]
-# Save the image
-cv2.imwrite(name + '_before.jpg', cv2.resize(img, video_resize))
-# Save the video
-fourcc = cv2.VideoWriter_fourcc(*'H264')
-output_video = cv2.VideoWriter(name + '_after.mp4', fourcc, 15, video_resize)
-for frame in frames:
-    output_video.write(frame)
-output_video.release()
+# # Crop the image to 1:1
+# img = img[:, img_crop:img_crop + img_size, :]
+# # Save the image
+# cv2.imwrite(name + '_before.jpg', cv2.resize(img, video_resize))
+# # Save the video
+# fourcc = cv2.VideoWriter_fourcc(*'H264')
+# output_video = cv2.VideoWriter(name + '_after.mp4', fourcc, 15, video_resize)
+# for frame in frames:
+#     output_video.write(frame)
+# output_video.release()
 
-# image = cv2.imread('motion_canvas.png')
-# image_crop = int((image.shape[1] - image.shape[0]) / 2)
-# image = image[:, image_crop:image_crop + image.shape[0], :]
-# image = cv2.resize(image, (256, 256))
-# cv2.imwrite('motion_canvas_before.jpg', image)
+image = cv2.imread('dage.png')
+image_crop = int((image.shape[1] - image.shape[0]) / 2)
+image = image[:, image_crop:image_crop + image.shape[0], :]
+image = cv2.resize(image, (256, 256))
+cv2.imwrite('dage_before.jpg', image)
